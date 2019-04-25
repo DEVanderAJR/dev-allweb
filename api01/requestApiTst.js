72d07ad7a738c93fdca79ff8bd117f19411d1f4d
Vanderlei Assuncao Junior - Grupo Orguel <vanderlei.junior@GRUPOORGUEL.COM.BR>
2019-04-25 15:38:32 -0300

    Ajsutes e complementos.

--- api01/requestApiTst.js
+++ api01/requestApiTst.js
@@ -1,19 +1,20 @@
 /*
 *Exemplo de consumo de api externa com NODE
-*
+*Teste API NODE*/
 
-Teste API NODE*/
 var url =  'https://jsonplaceholder.typicode.com/posts'; // api externa para teste
 var request = require('request');
     request(url, function (error, response, body) {        
         console.log('error: ', error); //Exibe erros caso ocorra;
-        console.log('status: ', response && response.statuscode);// Exibe codigo do status da conex?o
+        console.log('status: ', response && response.statuscode);// Exibe codigo do status da conexão
       
         var posts = JSON.parse(body);
 
         posts.forEach(element => {
+           console.log("Id: "+ element['id']);
            console.log("Titulo: "+ element['title']);
-            
+           console.log("Descricao: "+ element['body']);
+                       
         });
         
     });
